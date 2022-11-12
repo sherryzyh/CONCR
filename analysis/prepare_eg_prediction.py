fd67c3f42e96f9cf2fb88d5c4cc44b7c4ff26285
floshen18 <88593707+floshen18@users.noreply.github.com>
2022-11-12 17:56:37 -0500

    Remove irrelevant analysis outputs

--- analysis/prepare_eg_prediction.py
+++ analysis/prepare_eg_prediction.py
@@ -10,9 +10,9 @@ if __name__ == '__main__':
 
     output = {}
     for i, line in enumerate(lines):
-        output[f'dev-{i}'] = line.split(".,")[-1].strip("\n")
+        output[f'dev-{i}'] = line.strip("\n")
 
-    output_file = "_".join(input_file.split("_")[:-2] + ["prediction.json"])
+    output_file = "_".join(input_file.split("_")[:-1] + ["prediction.json"])
     output_path = os.path.join('prediction_for_eval', output_file)
     with open(output_path, "w", encoding="utf-8") as fp:
         json.dump(output, fp)
