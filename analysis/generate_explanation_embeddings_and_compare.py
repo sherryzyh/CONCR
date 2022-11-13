1b08bb1bc64f924aa65549688e767c6756017338
Ubuntu <ubuntu@ip-172-31-25-135.us-east-2.compute.internal>
2022-11-13 19:35:16 +0000

    Add generated embeddings and similarities

--- analysis/generate_explanation_embeddings_and_compare.py
+++ analysis/generate_explanation_embeddings_and_compare.py
@@ -1,23 +1,37 @@
 import json
 import pickle
+import sys
+import csv
 from sentence_transformers import SentenceTransformer, util
 
 if __name__ == '__main__':
-    eg_explanation_file = 'gpt2_eg_epoch_6_explanations.csv'
-    cr_eg_explanation_file = 'gpt2_cr_eg_epoch_8_explanations.csv'
-    with open('raw_output/' + eg_explanation_file, 'r') as f:
+    input_file = sys.argv[1]
+    with open('raw_output/' + input_file, 'r') as f:
         lines = f.readlines()
-        eg_explanations = [line.strip("\n") for line in lines]
-    with open('raw_output/' + cr_eg_explanation_file, 'r') as f:
-        lines = f.readlines()
-        cr_eg_explanations = [line.strip("\n") for line in lines]
-    print("CR and CR-EG explanations loaded")
+        generated_explanations = [line.strip("\n") for line in lines]
+    print("Generated explanations loaded")
 
-    eg_explanation_embeddings = model.encode(eg_explanations)
-    cr_eg_explanation_embeddings = model.encode(cr_eg_explanations)
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    generated_explanation_embeddings = model.encode(generated_explanations)
 
-    with open('embeddings/eg_explanation_embeddings.pkl', "wb") as fOut:
-        pickle.dump({'eg_explanations': eg_explanations, 'embeddings': eg_explanation_embeddings}, fOut, protocol=pickle.HIGHEST_PROTOCOL)
-    with open('embeddings/cr_eg_explanation_embeddings.pkl', "wb") as fOut:
-        pickle.dump({'cr_eg_explanations': cr_eg_explanations, 'embeddings': cr_eg_explanation_embeddings}, fOut, protocol=pickle.HIGHEST_PROTOCOL)
+    output_file = input_file.split(".")[0]
+    with open('embeddings/' + output_file + '_embeddings.pkl', "wb") as fOut:
+        pickle.dump({output_file: generated_explanations, 'embeddings': generated_explanation_embeddings}, fOut, protocol=pickle.HIGHEST_PROTOCOL)
+    print("Generated explanations saved")
+
+    #Load sentences & embeddings from disc
+    with open('embeddings/explanation_embeddings.pkl', "rb") as fIn:
+        stored_data = pickle.load(fIn)
+        provided_explanation_embeddings = stored_data['embeddings']
+    print("Provided explanation embeddings loaded")
+
+    similarities = []
+    for i in range(len(provided_explanation_embeddings)):
+        cos_sim = util.cos_sim(generated_explanation_embeddings[i], provided_explanation_embeddings[i])
+        similarities.append(cos_sim.item())
+    print("Explanation embedding similarities generated")
     
+    with open('processed_output/' + output_file + '_similarities.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows([[s] for s in similarities])
+    print("Explanation embedding similarities saved")
