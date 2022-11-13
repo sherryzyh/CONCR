ea5107f931ee28027285c83db5650a0080f58393
floshen18 <88593707+floshen18@users.noreply.github.com>
2022-11-13 16:12:56 -0500

    Add micro evaluation scripts and results

--- analysis/cr_micro_evaluation.py
+++ analysis/cr_micro_evaluation.py
@@ -1,43 +1,42 @@
 import jsonlines
-import json
 import sys
+import os
+import csv
 
 
 def read_gold_data(path):
     fi = jsonlines.open(path, 'r')
-    labels = {line['index']: line['label'] for line in fi}
+    labels = [line['label'] for line in fi]
     return labels
 
 
-def evaluation_metrics(gold, predictions):
-    count = 0
-    for key, value in predictions.items():
-        try:
-            gold_label = gold[key]
-        except:
-            raise KeyError('{} is not a correct index in the dataset.')
-
-        if gold_label == value:
-            count += 1
-        else:
-            continue
-
-    accuracy = count / len(gold)
-    return accuracy
+def micro_evaluation_accuracy(gold, prediction):
+    if gold == int(prediction):
+        return 1
+    return 0
 
 
 def main():
     prediction_file = sys.argv[1]
-    gold_file = sys.argv[2]
+    gold_path = '../data/dev_full_augmented.jsonl'
 
-    predictions = json.load(open(prediction_file, 'r'))
-    gold_labels = read_gold_data(gold_file)
+    # load predictions
+    with open('raw_output/' + prediction_file, 'r') as f:
+        predictions = f.readlines()
 
-    fo = open('./prediction_causal_reasoning.json', 'w')
+    # load gold labels
+    gold_labels = read_gold_data(gold_path)
 
-    accuracy = evaluation_metrics(gold_labels, predictions)
-    json.dump({"accuracy": accuracy}, fo)
-    print("[Accuracy]: {}".format(accuracy))
+    output_list = []
+    for i in range(len(gold_labels)):
+        accuracy = micro_evaluation_accuracy(gold_labels[i], predictions[i])
+        output_list.append(accuracy)
+
+    output_file = "_".join(prediction_file.split("_")[:-1] + ["micro", "CR.csv"])
+    output_path = os.path.join('micro_evaluation', output_file)
+    with open(output_path, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows([[a] for a in output_list])
 
 
 if __name__ == '__main__':
--- analysis/eg_micro_evaluation.py
+++ analysis/eg_micro_evaluation.py
@@ -8,62 +8,54 @@ import os
 
 def read_gold_data(path):
     fi = jsonlines.open(path, 'r')
-    explanations = {line['index']: line['conceptual_explanation'] for line in fi}
+    explanations = [line['conceptual_explanation'] for line in fi]
     return explanations
 
 
-def micro_evaluation_bleu(golds, predictions, output_dict):
-    for key in predictions:
-        prediction = predictions[key]
-        try:
-            gold = golds[key]
-        except:
-            raise KeyError('{} is not a correct index in e-CARE dataset'.format(key))
-
-        bleu_score = bleu([gold], prediction)
-        output_dict[key]["bleu"] = bleu_score
-
-    return output_dict
+def micro_evaluation_bleu(gold, prediction):
+    avg_bleu = bleu(gold, prediction)
+    return avg_bleu
 
 
-def micro_evaluation_rouge(golds, predictions, output_dict):
+def micro_evaluation_rouge(gold, prediction):
     rouge = Rouge()
-
-    for key in predictions:
-        prediction = predictions[key]
-        try:
-            gold = golds[key]
-        except:
-            raise KeyError('{} is not a correct index in e-CARE dataset'.format(key))
-
-        try:
-            scores = rouge.get_scores(prediction, gold)
-            rouge_l = scores[0]['rouge-l']['r']
-            output_dict[key]["rouge_l"] = rouge_l
-        except:
-            continue
-
-    return output_dict
+    scores = rouge.get_scores(prediction, gold)
+    rouge1 = scores[0]['rouge-1']['r']
+    rouge2 = scores[0]['rouge-2']['r']
+    rougel = scores[0]['rouge-l']['r']
+    return rouge1, rouge2, rougel
 
 
 def main():
     prediction_file = sys.argv[1]
-    gold_path = sys.argv[2]
+    cos_sim_file = prediction_file.split(".")[0] + '_similarities.csv'
+    gold_path = '../data/dev_full_augmented.jsonl'
 
-    prediction_path = os.path.join('prediction_for_eval', prediction_file)
-    predictions = json.load(open(prediction_path, 'r'))
+    # load predictions
+    with open('raw_output/' + prediction_file, 'r') as f:
+        predictions = f.readlines()
+
+    # load gold labels (provided explanations)
     gold_labels = read_gold_data(gold_path)
 
-    output_dict = {k:dict(prediction=v) for k, v in predictions.items()}
-    output_dict = micro_evaluation_bleu(gold_labels, predictions, output_dict)
-    output_dict = micro_evaluation_rouge(gold_labels, predictions, output_dict)
+    # load cosine similarities between generated and provided explanations
+    with open('processed_output/' + cos_sim_file, 'r') as f:
+        similarities = f.readlines()
+
+    output_dict_list = []
+    for i in range(len(gold_labels)):
+        avg_bleu = micro_evaluation_bleu(gold_labels[i], predictions[i])
+        rouge1, rouge2, rougel = micro_evaluation_rouge(gold_labels[i], predictions[i])
+        avg_rouge = sum([rouge1, rouge2, rougel]) / 3
+        output_dict_list.append(dict(
+            avg_bleu=avg_bleu, 
+            rouge1=rouge1, rouge2=rouge2, rougel=rougel, avg_rouge=avg_rouge, 
+            cos_sim=float(similarities[i])))
 
     output_file = "_".join(prediction_file.split("_")[:-1] + ["micro", "evaluation.json"])
-    output_path = os.path.join('evaluation_result', output_file)
+    output_path = os.path.join('micro_evaluation', output_file)
     fo = open(output_path, 'w')
-
-    json.dump(output_dict, fo)
-
+    json.dump(output_dict_list, fo)
 
 if __name__ == '__main__':
     main()
