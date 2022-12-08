contrastive causal learning w/ cossim fix bug

code/model/contrastive_discriminate_model.py
@@ -44,6 +44,8 @@ class CosSimilarity(nn.Module):
         self.cos = nn.CosineSimilarity(dim=-1)
 
     def forward(self, x, y):
+        x = x.unsqueeze(1)
+        y = y.unsqueeze(0)
         return self.cos(x, y) / self.temp
 
 
@@ -67,7 +69,8 @@ class Scorer(nn.Module):
         self.init_weights()
 
     def forward(self, x, y):
-        pair = torch.cat([x, y], dim=-1)
+        # x, y [batch_size, hidden_size]
+        pair = self.pairxy(x, y)
         score = self.score_mlp(pair).squeeze()
         return score / self.temp
 
@@ -76,6 +79,20 @@ class Scorer(nn.Module):
             if isinstance(m, nn.Linear):
                 nn.init.xavier_uniform_(m.weight)
                 m.bias.data.fill_(0.01)
+
+    def pairxy(self, x, y):
+        batch_size = x.size(0)
+        hidden_size = x.size(1)
+        idx_0 = torch.arange(batch_size)
+        idx_1 = torch.arange(batch_size)
+        idx_0, idx_1 = torch.meshgrid(idx_0, idx_1)
+        # idx_0 = [[0, 0], [1, 1]]
+        # idx_1 = [[0, 1], [0, 1]]
+        idx_0 = idx_0.view(-1)
+        idx_1 = idx_1.view(-1)
+        xypair = torch.cat([x[idx_0],y[idx_1]], dim=1) # [batch_size * batch_size, hidden_size * 2]
+        xypair = xypair.view(batch_size, batch_size, -1) # [batch_size, batch_size, hidden_size * 2]
+        return xypair
 
 
 class contrastive_reasoning_model(nn.Module):
@@ -161,7 +178,7 @@ class contrastive_reasoning_model(nn.Module):
         causes_0, effects_0 = self.compose_causal_pair(premise, hypothesis_0, labels)
         causes_0 = causes_0.to(input_ids.device)
         effects_0 = effects_0.to(input_ids.device)
-        contrastive_causal_score = self.sim(causes_0.unsqueeze(1), effects_0.unsqueeze(0))
+        contrastive_causal_score = self.sim(causes_0, effects_0)
         # print("contrastive score:", contrastive_causal_score.size())
         # print(contrastive_causal_score)
 
@@ -171,7 +188,7 @@ class contrastive_reasoning_model(nn.Module):
             causes_1, effects_1 = self.compose_causal_pair(premise, hypothesis_1, labels)
             causes_1 = causes_1.to(input_ids.device)
             effects_1 = effects_1.to(input_ids.device)
-            hardneg_causal_score = self.sim(causes_1.unsqueeze(1), effects_1.unsqueeze(0))
+            hardneg_causal_score = self.sim(causes_1, effects_1)
 
             # print("contrastive score:", contrastive_causal_score.size())
             # print("hardneg_causal_score:", hardneg_causal_score.size())
@@ -235,13 +252,17 @@ class contrastive_reasoning_model(nn.Module):
 
         premise_0_score_matrix = self.sim(causes_0.unsqueeze(1), effects_0.unsqueeze(0))
         score_0 = torch.diagonal(premise_0_score_matrix, offset=0)
-        print("score 0.size:", score_0)
+        # print("score 0.size:", score_0)
 
         causes_1, effects_1 = self.compose_causal_pair(premise, hypothesis_1, labels)
         causes_1 = causes_1.to(input_ids.device)
         effects_1 = effects_1.to(input_ids.device)
         premise_1_score_matrix = self.sim(causes_1.unsqueeze(1), effects_1.unsqueeze(0))
         score_1 = torch.diagonal(premise_1_score_matrix, offset=0)
-        print("score 1.size:", score_1)
+        # print("score 1.size:", score_1)
+
+        score = torch.cat([score_0, score_1], dim=1)
+        # print("score size:", score.size())
+        # print(torch.argmax(score, dim=1))
 
         return score_0, score_1

code/train_cl_discriminate.py
@@ -55,11 +55,11 @@ def parse_hps():
     parser.add_argument('--score', type=str, default="cossim", help="scorer type")
     parser.add_argument('--hard_negative_weight', type=float, default=0.0, help="hard negative weight")
 
-
     # parsing the hyper-parameters from command line and define logger
     hps = parser.parse_args()
     hps.nowtime = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
     return hps
+
 
 def evaluate(model, dev_dataloader, patient, best_accuracy, loss_function, logger, hps):
     model.eval()
@@ -85,12 +85,12 @@ def evaluate(model, dev_dataloader, patient, best_accuracy, loss_function, logge
             if not os.path.exists(hps.save_dir):
                 os.mkdir(hps.save_dir)
             logger.info("[Saving] Saving Model to {}".format(hps.save_dir))
-            if hps.hyp_only:
-                torch.save(model, os.path.join(hps.save_dir, 'discriminate_'+ hps.model_name + '_hyp'))
-            else:
-                torch.save(model, os.path.join(hps.save_dir, 'discriminate_'+ hps.model_name))
-            # torch.save(model, os.path.join(hps.save_dir, exp_name))
-                
+            # if hps.hyp_only:
+            #     torch.save(model, os.path.join(hps.save_dir, exp_name + '_hyp'))
+            # else:
+            #     torch.save(model, os.path.join(hps.save_dir, exp_name))
+            torch.save(model, os.path.join(hps.save_dir, exp_name))
+
         else:
             patient += 1
 
@@ -99,6 +99,7 @@ def evaluate(model, dev_dataloader, patient, best_accuracy, loss_function, logge
             logger.info("[INFO] Stopping Training by Early Stopping")
             stop_train = True
     return patient, stop_train
+
 
 def CL_train(model, optimizer, train_dataloader, dev_dataloader, loss_function, logger, hps):
     logger.info("[INFO] Start Training")
@@ -131,18 +132,19 @@ def CL_train(model, optimizer, train_dataloader, dev_dataloader, loss_function, 
             if i == 0:
                 init_loss = loss.item() / len(batch)
             last_loss = loss.item() / len(batch)
-            t.set_postfix(avg_loss='{}'.format(total_loss/(epoch_step+1)))
+            t.set_postfix(avg_loss='{}'.format(total_loss / (epoch_step + 1)))
             epoch_step += 1
 
             loss.backward()
             optimizer.step()
 
             if hps.evaluation_strategy == "step" and step % hps.evaluation_step == 0 and step != 0:
-                patient, stop_train = evaluate(model, dev_dataloader, patient, best_accuracy, loss_function, logger, hps)
+                patient, stop_train = evaluate(model, dev_dataloader, patient, best_accuracy, loss_function, logger,
+                                               hps)
                 if stop_train:
                     return
             step += 1
-        
+
         print(f"In Epoch {epoch} training, individual loss {init_loss:.4f} -> {last_loss:.4f}")
         if hps.evaluation_strategy == "epoch":
             patient, stop_train = evaluate(model, dev_dataloader, patient, best_accuracy, loss_function, logger, hps)
@@ -157,10 +159,11 @@ def load_loss_function(hps):
         loss_function = nn.BCEWithLogitsLoss(reduction='mean')
     return loss_function
 
+
 def main():
     # parse hyper parameters
     hps = parse_hps()
-    exp_name = "discriminate_" + hps.model_name
+    exp_name = "discriminate_" + hps.model_dir
     if hps.save_name is not None:
         exp_name = hps.save_name + "_" + exp_name
     if hps.hyp_only:
@@ -194,8 +197,9 @@ def main():
 
     # contrastive Tokenization
     logger.info("[DATA] Tokenization and Padding for Data")
-    train_ids, train_mask, train_seg_ids, train_labels, train_length = contrastive_tokenize(train_data, hps, loading_mode="train")
-    dev_ids, dev_mask, dev_seg_ids, dev_labels, dev_length = contrastive_tokenize(dev_data, hps, loading_mode="dev")
+    train_ids, train_mask, train_seg_ids, train_labels, train_length = contrastive_tokenize(train_data, hps,
+                                                                                            loading_mode="train")
+    dev_ids, dev_mask, dev_seg_ids, dev_labels, dev_length = contrastive_tokenize(dev_data, hps, loading_mode="train")
     # print("tokenzied data:", len(dev_ids))
     # print("\tdev_ids:", dev_ids[0])
     # print("\tdev_mask:", dev_mask[0])
@@ -232,10 +236,6 @@ def main():
     # contrastive training
     CL_train(model, optimizer, train_dataloader, dev_dataloader, loss_function, logger, hps)
 
+
 if __name__ == '__main__':
     main()
-
-
-
-
-