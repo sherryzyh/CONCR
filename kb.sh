5926f2c9cb31946e94c86b039c9a0a5ffee9e6a7
Ubuntu <ubuntu@ip-172-31-25-135.us-east-2.compute.internal>
2022-12-09 02:57:30 +0000

    Add kb.sh

---
+++ kb.sh
@@ -0,0 +1,20 @@
+python3 train_discriminate.py \
+  --data_dir "../data/Causal_Reasoning/" \
+  --model_dir "../../huggingface_transformers/bert-base-cased/" \
+  --save_dir "./output/saved_model" \
+  --log_dir "./output/log" \
+  --train "train.jsonl" \
+  --dev "dev.jsonl" \
+  --test "test.jsonl" \
+  --model_name "bert" \
+  --gpu "0" \
+  --batch_size 2 \
+  --cuda True\
+  --epochs 10 \
+  --evaluation_step 250 \
+  --lr 1e-5 \
+  --set_seed True \
+  --seed 338 \
+  --patient 3 \
+  --loss_func "BCE" \
+  --with_kb True
