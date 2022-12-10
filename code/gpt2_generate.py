0f19f6ad30d02245822383a86259dfa896eaedcb
yennifer98 <yennifer_98@163.com>
2022-12-10 10:36:43 -0500

    testing eg

--- code/gpt2_generate.py
+++ code/gpt2_generate.py
@@ -121,6 +121,7 @@ def main():
                 batch = tuple(term.cuda() for term in batch)
 
             input_ids, input_mask, input_seg_ids, input_labels, input_labels_mask = batch
+            print(input_mask)
             # pdb.set_trace()
             tmp = torch.ones(input_labels_mask.shape).long()
             count_mask_length = torch.sum(tmp==input_labels_mask.cpu(), 1).squeeze().tolist()
